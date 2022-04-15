f0dfa790be5bbc72def261cbcc5c05d957d8f340
Caeta-ok <joaquincaeta@gmail.com>
2022-04-15 11:12:18 -0300

    Boilerplate updated

--- rollup.config.js
+++ rollup.config.js
@@ -12,12 +12,7 @@ export default [
     ],
     output: [
       { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "esm" },
-      {
-        file: "example/src/reactComponentLib/index.js",
-        format: "es",
-        banner: "/* eslint-disable */"
-      }
+      { file: pkg.module, format: "esm" }
     ]
   }
 ];
